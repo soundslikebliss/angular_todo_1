31f7b9a432082e6979b8a245a3fb242f1d112daa
David <russianid1@gmail.com>
2014-10-19 22:35:32 -0700

    first commit

---
+++ todoController.js
@@ -0,0 +1,24 @@
+var todo = angular.module('todo', []);
+
+todo.controller('todoController', ['$scope', function ($scope) {
+
+
+	$scope.tasks = [
+		{
+			// 'number': 1,
+			'title': 'adopt a bunny',
+			// 'description': 'go to the shelter and fill out the necessary paperwork to get your bunny'
+		},
+		{
+			// 'number': 2,
+			'title': 'buy bunny food',
+			// 'description': 'go to Fred Meyer in the pet dept and find something grassy'
+		}
+	];
+
+	$scope.addTask = function() {
+		$scope.tasks.push({title:$scope.formTask});
+	}
+
+}]);
+
